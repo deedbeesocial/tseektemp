cb7959634415ee7c13ec8e63179f4c2b9ac68aca
JEREMIA-SUNNY <you@example.com>
2022-07-20 12:14:07 +0530

    new color of bg changed

--- components/testimonial.js
+++ components/testimonial.js
@@ -59,14 +59,14 @@ export default function Testimonials() {
   ];
 
   return (
-    <section className="text-black  body-font">
+    <section className="text-black  bg-slate-300 body-font">
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-wrap w-full mb-20">
           <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
               testimonial
             </h1>
-            <div className="h-1 w-20 bg-green-300 rounded"></div>
+            <div className="h-1 w-20 bg-black rounded"></div>
           </div>
         </div>
         <div className="flex flex-wrap -m-4 ">
