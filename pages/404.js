ca33e6a83a7242c335565fdb4ed8d869b8df95a5
JEREMIA-SUNNY <you@example.com>
2022-07-29 15:17:53 +0530

    new capabilities images added

--- pages/404.js
+++ pages/404.js
@@ -4,16 +4,7 @@ function Pagenotfound(){
     return(
         <>
         <div className="w-screen h-screen  flex justify-center lg:flex-row flex-col  bg-blue ">
-            <div className="relative w-1/2 h-[600px] rounded-lg">
-                <Image
-                    layout="fill"
-                    src={'/404/404.jpg'}
-                    alt="404"
-                    objectFit="cover"
-
-
-                />
-            </div>
+            
 
             <div className="lg:mt-[10%]">
                 <p className="text-6xl text-white font-extrabold items-center font-mono">Page Not Found 404</p>
--- pages/capabilities.js
+++ pages/capabilities.js
@@ -2,23 +2,39 @@ import Image from "next/image";
 function Capabilities() {
   return (
     <>
+    <div className="w-full  h-72 relative ">
+          <Image
+            layout="fill"
+            alt="ser"
+            src="/capabi/capablities.png"
+            objectFit="cover "
+          />
+          <div className="w-full    h-full  flex  justify-center">
+            <div className="text-white relative   h-full flex justify-center items-center">
+              <h1 className=" text-4xl   text-bold  w-full     text-white">
+              Capabilities
+              </h1>
+            </div>
+          </div>
+        </div>
       <section
         className="pt-12 pb-10 text-zinc-900 bg-coustom1"
         id="capabilities"
       >
-        <div className="md:container p-4 mx-auto" id="Consulting">
-          <h1 className="text-4xl tracking-wide font-light text-center  text-black">
-            Capabilities
-          </h1>
 
-          <div className="grid md:grid-cols-2 grid-cols-1 md:gap-x-20 gap-4 md:pt-12 pt-4">
+        
+
+
+        <div className="md:container p-4 mx-auto" id="Consulting">
+        
+          <div className="grid md:grid-cols-2 grid-cols-1 md:gap-x-20 gap-4 md:pt- pt-4">
             <div className="w-full h-[50px] lg:col-span-2  col-span-1 "></div>
             <div className="w-auto h-48 md:h-auto relative">
               <Image
                 layout="fill"
                 className="absolute h-full w-full object-cover"
                 alt=""
-                src="/caps/cap1.jpg"
+                src="/capabi/consulting.png"
               />
             </div>
 
@@ -69,7 +85,7 @@ function Capabilities() {
                 layout="fill"
                 className="absolute h-full w-full object-cover"
                 alt=""
-                src="/caps/cap2.jpg"
+                src="/capabi/analytics.png"
               />
             </div>
             <div className="w-full h-[100px] lg:col-span-2  col-span-1 " id="Mobility"></div>
@@ -79,7 +95,7 @@ function Capabilities() {
                 layout="fill"
                 className="absolute h-full w-full object-cover"
                 alt=""
-                src="/caps/cap3.jpg"
+                src="/capabi/mobility.png"
               />
             </div>
 
@@ -131,7 +147,7 @@ function Capabilities() {
                 layout="fill"
                 className="absolute h-full w-full object-cover"
                 alt=""
-                src="/caps/cap4.jpg"
+                src="/capabi/cloud.png"
               />
             </div>
             <div className="w-full h-[100px] lg:col-span-2  col-span-1 " id="Outsourcing"></div>
@@ -140,7 +156,7 @@ function Capabilities() {
                 layout="fill"
                 className="absolute h-full w-full object-cover"
                 alt=""
-                src="/caps/cap1.jpg"
+                src="/capabi/outsourcing.png"
               />
             </div>
 
@@ -191,7 +207,7 @@ function Capabilities() {
                 layout="fill"
                 className="absolute h-full w-full object-cover"
                 alt=""
-                src="/caps/cap2.jpg"
+                src="/capabi/upskilling.png"
               />
             </div>
             <div className="w-full h-[100px] lg:col-span-2  col-span-1 " id="Staffing"></div>
@@ -200,7 +216,7 @@ function Capabilities() {
                 layout="fill"
                 className="absolute h-full w-full object-cover"
                 alt=""
-                src="/caps/cap3.jpg"
+                src="/capabi/staffing.png"
               />
             </div>
 
