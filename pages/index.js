81d9dac9fc137830010bdb6d621957b65ebc1451
JEREMIA-SUNNY <you@example.com>
2022-11-08 15:21:01 +0530

    new changes for the image

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,9 @@ export default function Home() {
         <meta name="description" content="Official website for tSeek" />
         <link rel="icon" href="favicon.ico " />
       </Head>
-      <img src="/mainimage.png" className="w-screen h-screen" alt="" />
+      <div className="bg-[rgba(243,122,70,255)] w-screen h-screen flex justify-center items-center mx-auto">
+        <img src="/mainimage.png" className="w-[1400px] h-[900px]" alt="" />
+      </div>
     </>
   );
 }
